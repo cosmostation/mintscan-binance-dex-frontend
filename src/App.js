bedfa144b33ed607c4c687b084f8f7a99c4b867c
minami-choi <cmina2846@gmail.com>
2020-01-06 14:10:24 +0900

    mintscan project init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./App.scss"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Binance Explorer</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
